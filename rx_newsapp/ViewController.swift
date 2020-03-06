f070462c0f409829fee10a67b5a75ee254af3aad
Dzmitry  Sakalouski <dzmitrysakalouski@MacBook-Pro-Dzmitry.local>
2020-03-06 15:50:13 +0300

    init

---
+++ rx_newsapp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  rx_newsapp
+//
+//  Created by Dzmitry  Sakalouski  on 3/6/20.
+//  Copyright © 2020 Dzmitry  Sakalouski . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
